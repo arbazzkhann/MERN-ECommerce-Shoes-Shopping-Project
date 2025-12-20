51b18242aed954c819a9afcc477764c86ccad2cc
Arbaz Khan <arbazkha78az@gmail.com>
2025-12-20 20:28:54 +0530

    navbar - adding NavLinks, isLogin logics

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from "./Modal";
 import InputForm from './InputForm';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  let token = localStorage.getItem("token");
+
+  const [isLogin, setIsLogin] = useState(token ? true : false);
+
+  useEffect(() => {
+    setIsLogin(token ? true : false);
+  }, [token])
 
   const checkLogin = () => {
-    setIsOpen(!isOpen);
+    if(token) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("_id");
+      localStorage.removeItem("email");
+      setIsOpen(true);
+    }
+    else {
+      setIsOpen(true);
+    }
   }
 
   return (
@@ -14,14 +30,16 @@ const Navbar = () => {
         <header className='color-white'>
             <h2>Shoes</h2>
             <ul>
-                <li>Home</li>
-                <li>Shoes</li>
-                <li>Favourites</li>
-                <li onClick={checkLogin}>Login</li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li onClick={() => !isLogin && setIsOpen(true)}><NavLink to={ isLogin ? "/shoes" : "/" }>Shoes</NavLink></li>
+                <li onClick={() => !isLogin && setIsOpen(true)}><NavLink to={ isLogin ? "/favourites" : "/" }>Favourites</NavLink></li>
+                <li onClick={checkLogin}>{(isLogin) ? "Logout" : "Login"}</li>
             </ul>
         </header>
         { isOpen && <Modal onClose={() => setIsOpen(false)}>
-                      <InputForm/>  {/*passing as a child*/}
+                      <InputForm
+                        setIsOpen={() => setIsOpen(false)}
+                      />  {/*passing as a child*/}
                     </Modal>           
         }
     </>
